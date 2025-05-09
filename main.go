226bce09a48fef8b9a846040f5021a885445922a
i9 <oluwarinolasam@gmail.com>
2025-05-09 16:49:22 +0100

    update

--- main.go
+++ main.go
@@ -15,4 +15,12 @@ func main() {
 	fmt.Print("medianOfTwoSortedArrays([]int{1, 3}, 2, []int{2, 4}, 2): ")
 	fmt.Println(medianOfTwoSortedArrays([]int{1, 3}, 2, []int{2, 4}, 2)) // Answer: 2.5
 
+	fmt.Print("medianOfTwoSortedArrays([]int{1}, 1, []int{2, 4}, 2): ")
+	fmt.Println(medianOfTwoSortedArrays([]int{1}, 1, []int{2, 4}, 2)) // Answer: 2
+
+	fmt.Print("medianOfTwoSortedArrays([]int{2,4}, 2, []int{2, 4}, 2): ")
+	fmt.Println(medianOfTwoSortedArrays([]int{2, 4}, 2, []int{2, 4}, 2)) // Answer: 3
+
+	fmt.Print("medianOfTwoSortedArrays([]int{2,4}, 2, []int{}, 0): ")
+	fmt.Println(medianOfTwoSortedArrays([]int{2, 4}, 2, []int{}, 0)) // Answer: 3
 }
